7552eb9d6fa87c106535d5f24ca078d4322b7cc7
studentcoll <studentcoll@tp.local>
2026-09-17 14:27:47 +0300

    Add CTA analytics event

--- js/script.js
+++ js/script.js
@@ -18,11 +18,9 @@ const programCta = document.querySelector('#program-cta');
 if (programCta) {
   programCta.addEventListener('click', () => {
     document.querySelector('#program-preview').hidden = false;
-    if (typeof gtag === 'function') {
-      gtag('event', 'cta_click', {
-        button_name: 'program',
-        page_section: 'hero'
-      });
-    }
+    gtag('event', 'cta_click', {
+      button_name: 'program',
+      page_section: 'hero'
+    });
   });
 }
